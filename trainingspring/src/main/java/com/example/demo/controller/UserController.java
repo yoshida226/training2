23f4d37ba3e4363cdef524039523bd0796eadb0a
mmyy0 <mmyy0@192.168.0.160>
2025-04-04 11:58:22 +0900

    【STEP９】MyBathisとMySQLで連携

--- trainingspring/src/main/java/com/example/demo/controller/UserController.java
+++ trainingspring/src/main/java/com/example/demo/controller/UserController.java
@@ -1,21 +1,14 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.example.demo.dto.UserUpdateRequest;
+import com.example.demo.dto.UserSearchRequest;
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
 
@@ -24,87 +17,33 @@ import com.example.demo.service.UserService;
  */
 @Controller
 public class UserController {
-
   /**
    * ユーザー情報 Service
    */
   @Autowired
-  private UserService userService;
+  UserService userService;
 
   /**
-   * ユーザー情報一覧画面を表示
+   * ユーザー情報検索画面を表示
    * @param model Model
    * @return ユーザー情報一覧画面
    */
-  @GetMapping(value = "/user/list")
-  public String displayList(Model model) {
-    List<User> userlist = userService.searchAll();
-    model.addAttribute("userlist", userlist);
-    return "user/list";
+  @GetMapping(value = "/user/search")
+  public String displaySearch(Model model) {
+    model.addAttribute("userSearchRequest", new UserSearchRequest());
+    return "user/search";
   }
 
   /**
-   * ユーザー新規登録画面を表示
+   * ユーザー情報検索
+   * @param userSearchRequest リクエストデータ
    * @param model Model
    * @return ユーザー情報一覧画面
    */
-  @GetMapping(value = "/user/add")
-  public String displayAdd(Model model) {
-    return "user/add";
-  }
-
-  /**
-   * ユーザー情報詳細画面を表示
-   * @param id 表示するユーザーID
-   * @param model Model
-   * @return ユーザー情報詳細画面
-   */
-  @GetMapping("/user/{id}")
-  public String displayView(@PathVariable Long id, Model model) {
-    User user = userService.findById(id);
-    model.addAttribute("userData", user);
-    return "user/view";
-  }
-
-  /**
-   * ユーザー編集画面を表示
-   * @param id 表示するユーザーID
-   * @param model Model
-   * @return ユーザー編集画面
-   */
-  @GetMapping("/user/{id}/edit")
-  public String displayEdit(@PathVariable Long id, Model model) {
-    User user = userService.findById(id);
-    UserUpdateRequest userUpdateRequest = new UserUpdateRequest();
-    userUpdateRequest.setId(user.getId());
-    userUpdateRequest.setName(user.getName());
-    userUpdateRequest.setPhone(user.getPhone());
-    userUpdateRequest.setAddress(user.getAddress());
-    model.addAttribute("userUpdateRequest", userUpdateRequest);
-    return "user/edit";
-  }
-
-  /**
-   * ユーザー更新
-   * @param userRequest リクエストデータ
-   * @param model Model
-   * @return ユーザー情報詳細画面
-   */
-  @RequestMapping(value = "/user/update", method = RequestMethod.POST)
-  public String update(@Validated @ModelAttribute UserUpdateRequest userUpdateRequest, BindingResult result, Model model) {
-
-    if (result.hasErrors()) {
-      List<String> errorList = new ArrayList<String>();
-
-      for (ObjectError error : result.getAllErrors()) {
-        errorList.add(error.getDefaultMessage());
-      }
-      model.addAttribute("validationError", errorList);
-      return "user/edit";
-    }
-
-    // ユーザー情報の更新
-    userService.update(userUpdateRequest);
-    return String.format("redirect:/user/%d", userUpdateRequest.getId());
+  @RequestMapping(value = "/user/id_search", method = RequestMethod.POST)
+  public String search(@ModelAttribute UserSearchRequest userSearchRequest, Model model) {
+    User user = userService.search(userSearchRequest);
+    model.addAttribute("userinfo", user);
+    return "user/search";
   }
 }
--- trainingspring/src/main/java/com/example/demo/service/UserService.java
+++ trainingspring/src/main/java/com/example/demo/service/UserService.java
@@ -1,54 +1,29 @@
 package com.example.demo.service;
 
-import java.util.Date;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.example.demo.dto.UserUpdateRequest;
+import com.example.demo.dto.UserSearchRequest;
 import com.example.demo.entity.User;
-import com.example.demo.repository.UserRepository;
+import com.example.demo.repository.UserMapper;
 
 /**
  * ユーザー情報 Service
  */
 @Service
-@Transactional(rollbackFor = Exception.class)
 public class UserService {
-  /**
-   * ユーザー情報 Repository
-   */
-  @Autowired
-  private UserRepository userRepository;
+    /**
+     * ユーザー情報 Mapper
+     */
+    @Autowired
+    private UserMapper userMapper;
 
-  /**
-   * ユーザー情報 全検索
-   * @return 検索結果
-   */
-  public List<User> searchAll() {
-    return userRepository.findAll();
-  }
-
-  /**
-   * ユーザー情報 主キー検索
-   * @return 検索結果
-   */
-  public User findById(Long id) {
-    return userRepository.findById(id).get();
-  }
-
-  /**
-   * ユーザー情報 更新
-   * @param user ユーザー情報
-   */
-  public void update(UserUpdateRequest userUpdateRequest) {
-    User user = findById(userUpdateRequest.getId());
-    user.setAddress(userUpdateRequest.getAddress());
-    user.setName(userUpdateRequest.getName());
-    user.setPhone(userUpdateRequest.getPhone());
-    user.setUpdateDate(new Date());
-    userRepository.save(user);
-  }
+    /**
+     * ユーザー情報検索
+　　　* @param userSearchRequest リクエストデータ
+     * @return 検索結果
+     */
+    public User search(UserSearchRequest userSearchRequest) {
+        return userMapper.search(userSearchRequest);
+    }
 }
